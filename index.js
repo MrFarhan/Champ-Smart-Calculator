ad9584108f6a386480a52be7857b7c65650ff168
MrFarhan <muhammadfarhan012345@gmail.com>
2019-08-04 22:33:38 +0500

    Minor changes

--- index.js
+++ index.js
@@ -10,142 +10,37 @@ admin.initializeApp();
 exports.webhook = functions.https.onRequest((request, response) => {
     var number01 = request.body.queryResult.parameters.number;
     var number02 = request.body.queryResult.parameters.number1;
-    var totalAdd = number01 + number02
-    var totalLess = number01 - number02
-    var totalMultiply = number01 * number02
-    var totalDivide = number01 / number02
-
-    
     switch (request.body.queryResult.action) {
         case "additions":
             response.send({
-                fulfillmentText:`addition of ${number01} and ${number02} is ${totalAdd} `
+                fulfillmentText: `addition of ${number01} and ${number02} is ${number01 + number02} `
             });
 
-            case "Subtraction":
+        case "Subtraction":
+            response.send({
+                fulfillmentText: `Subtraction of ${number01} and ${number02} is ${number01 - number02} `
+            });
+
+        case "multiplication":
+            response.send({
+                fulfillmentText: `Multiplication of ${number01} into ${number02} is ${number01 * number02} `
+            });
+
+        case "division":
+            if (number02 === 0) {
                 response.send({
-                    fulfillmentText:`Subtraction of ${number01} and ${number02} is ${totalLess} `
-                });
+                    fulfillmentText: `Can not divide with 0 - Result: Infinaty `
+                })
+            }
+            else {
+                response.send({
+                    fulfillmentText: `division of ${number01} into ${number02} is ${(number01 / number02).toFixed(3)} `
+                })
+            };
 
-                case "multiplication":
-                    response.send({
-                        fulfillmentText:`Multiplication of ${number01} into ${number02} is ${totalMultiply} `
-                    });
-
-                    case "division":
-                        response.send({
-                            fulfillmentText:`division of ${number01} into ${number02} is ${totalDivide} `
-                        });
-
+        default:
+            break;
     }
 
-    
+
 });
-
-//     let params = request.body.queryResult.parameters;
-//     switch (request.body.queryResult.action) {
-//         case "BookHotel":
-
-//             firestore.collection('orders').add(params)
-//                 .then(() => {
-//                     return response.send({
-//                         fulfillmentText:
-//                             `${params.name} your hotel booking request for ${params.type} room is forwarded for 
-//                 ${params.person} person(s), we will contact you on ${params.email} soon !!! `
-//                     });
-
-//                 })
-//                 .catch((e => {
-
-//                     console.log("error:", e)
-
-//                     response.send({
-//                         fulfillmentText: `something went wrong while writing to database . .`
-
-//                     });
-//                 }))
-//             break;
-
-
-//         case "ShowBookings":
-
-//             firestore.collection('orders').get()
-//                 .then((querySnapshot) => {
-//                     // console.log("query is : ",querySnapshot)
-//                     var orders = [];
-//                     querySnapshot.forEach((doc) => { orders.push(doc.data()) });
-//                     //now orders have sth like this [{...} ,{...} , {...} ,  ]
-//                     // Converting array to speech
-//                     var speech = `you have ${orders.length} orders \n Shall i Show you ?`;
-
-
-//                     return response.send({
-//                         fulfillmentText: speech
-//                     });
-//                 })
-//                 .catch((err) => {
-//                     console.log('error getting info ', err);
-
-//                     response.send({
-//                         fulfillmentText: "something went wrong when readig from database . "
-//                     })
-//                 })
-//             break;
-
-//         case "ShowBookingsYes":
-
-//             firestore.collection('orders').get()
-//                 .then((querySnapshot) => {
-//                     // console.log("query is : ",querySnapshot)
-//                     var showOrders = [];
-//                     querySnapshot.forEach((doc) => { showOrders.push(doc.data()) });
-//                     //now orders have sth like this [{...} ,{...} , {...} ,  ]
-//                     // Converting array to speech
-
-//                     var followSpeech = [];
-
-//                     showOrders.forEach((eachOrder, index) => {
-//                         followSpeech += `number ${index + 1} is ${eachOrder.type} rooms for ${eachOrder.person} persons, ordered by ${eachOrder.name} email address is ${eachOrder.email} \n \n`
-//                     })
-//                     console.log("follow speech console ", followSpeech);
-//                     return response.send({
-//                         fulfillmentText: followSpeech
-//                     });
-//                 })
-//                 .catch((err) => {
-//                     console.log('error getting info ', err);
-
-//                     response.send({
-//                         fulfillmentText: "something went wrong when readig from database . "
-//                     })
-//                 })
-//             break;
-
-//             // firestore.collection('orders').add(params)
-//             //     .then(() => {
-//             //         return response.send({
-//             //             fulfillmentText:
-//             //                 `${params.name} your hotel booking request for ${params.type} room is forwarded for 
-//             //     ${params.person} person(s), we will contact you on ${params.email} soon !!! `
-//             //         });
-
-//             //     })
-//             //     .catch((e => {
-
-//             //         console.log("error:", e)
-
-//             //         response.send({
-//             //             fulfillmentText: `something went wrong while writing to database . .`
-
-//             //         });
-//             //     }))
-//             // break;
-
-
-
-//         default:
-
-//     }
-
-// });
-
